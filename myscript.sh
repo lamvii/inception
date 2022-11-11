bb52707044eee0ecdc4bb55a5da077c4acddca55
lamvii <ael-idri@e2r5p11.1337.ma>
2022-11-11 15:23:25 +0100

    setting nginx done

---
+++ myscript.sh
@@ -0,0 +1,9 @@
+cd
+cd goinfre/
+mkdir com.docker.docker
+docker pull nginx
+docker pull maria db
+docker pull mariadb
+docker pull alpine
+docker pull wordpress
+docker image ls -a
---
+++ nginx/nginx_config.sh
@@ -0,0 +1,34 @@
+# !/bin/sh
+
+openrc
+touch /run/openrc/softlevel
+service nginx start
+mkdir /etc/nginx/ssl
+openssl req -new -x509 -nodes -days 365 -newkey rsa:2048 \
+	-keyout /etc/nginx/ssl/selfsigned.key -out /etc/nginx/ssl/selfsigned.crt \
+	-subj "/C=aa/ST=aa/L=aa/O=aa/OU=aa/CN=aa"
+
+
+cd etc/nginx/http.d/
+sed -i '7i		ssl_certificate /etc/nginx/ssl/selfsigned.crt;' default.conf
+sed -i '8i		ssl_certificate_key /etc/nginx/ssl/selfsigned.key;' default.conf
+sed -i '9i		ssl_session_timeout 1d;' default.conf
+sed -i '10i		ssl_session_cache shared:SharedNixCraftSSL:10m; ' default.conf
+sed -i '11i		ssl_session_tickets off;' default.conf
+sed -i '12i' default.conf
+sed -i '13i		# TLS 1.3 only' default.conf
+sed -i '14i		ssl_protocols TLSv1.3;' default.conf
+sed -i '15i		ssl_prefer_server_ciphers off;' default.conf
+sed -i '16i' default.conf
+
+sed -i  's/listen 80 default_server;/listen 443 ssl http2;/' default.conf
+sed -i 's/listen \[::\]:80 default_server;/listen \[::\]:443 ssl http2;/' default.conf
+
+
+# Make sure the nginx starts when system reboots:
+rc-update add nginx default
+
+nginx -t
+rc-service nginx restart
+
+tail -f > /dev/null
