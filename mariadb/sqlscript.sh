6589bcf70026f765148950c7f9fe30322a37ea94
lamvii <ael-idri@e2r5p14.1337.ma>
2022-11-13 16:57:15 +0100

    setting mariadb in progress

--- mariadb/sqlscript.sh
+++ mariadb/sqlscript.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+/etc/init.d/mariadb setup
+rc-service mariadb start
+# rc-service mariadb stop
 
 echo "1- change configuration"
 cd /etc/my.cnf.d/
@@ -6,17 +9,19 @@ cd /etc/my.cnf.d/
 sed -i  's/skip-networking/#skip-networking/' mariadb-server.cnf
 sed -i  's/#bind-address=0.0.0.0/bind-address=0.0.0.0/' mariadb-server.cnf
 sed -i	'11i port=3306' mariadb-server.cnf
-# sed -i	'12i socket=/var/run/mysqld' mariadb-server.cnf
+sed -i	'12i socket=/run/mysqld' mariadb-server.cnf
 
 echo "----------configuration done-----------" 
 
+
 echo "2- creat sqluser"
 
-echo "CREATE DATABASE yourdb;" |  mariadb -u root -e
-echo "CREATE USER 'hliwa'@'localhost' IDENTIFIED BY 'secret';" | mariadb -u root -e
-echo "GRANT ALL PRIVILEGES ON yourdb.* TO 'hliwa'@localhost;" | mariadb -u root -e
+echo "CREATE DATABASE yourdb;" |  mariadb -u root
+echo "CREATE USER 'hliwa'@'localhost' IDENTIFIED BY 'secret';" | mariadb -u root 
+echo "GRANT ALL PRIVILEGES ON yourdb.* TO 'hliwa'@localhost;" | mariadb -u root
 # To verify the right permissions you just granted to the user, run this command:
 # echo "SHOW GRANTS FOR 'hliwa'@localhost;" | mariadb -u root -e
-echo "FLUSH PRIVILEGES;" | mariadb -u root -e
+echo "FLUSH PRIVILEGES;" | mariadb -u root
 
 echo "----------configuration done-----------"
+mysqld_safe
