2063412d704fe0d97b575c2a2969e47b6fe01f21
Abdellatif Rochdi <arochdi@e2r6p15.1337.ma>
2022-11-15 12:43:43 +0100

    setup adminer

--- mariadb/auto_secure_install.sql
+++ mariadb/auto_secure_install.sql
@@ -1,6 +1,27 @@
-UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root';
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+-- DELETE FROM mysql.user WHERE User='';
+-- DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+-- DROP DATABASE IF EXISTS test;
+-- DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+
+CREATE DATABASE IF NOT EXISTS yourdb;
+
+CREATE USER 'hliwa'@'%' IDENTIFIED BY 'secret';
+
+GRANT ALL PRIVILEGES ON yourdb.* TO 'hliwa'@'%';
+
+USE yourdb;
+
+UPDATE mysql.user SET Host='%' WHERE Host='localhost' AND User='root';
+UPDATE mysql.db SET Host='%' WHERE Host='localhost' AND User='root';
+FLUSH PRIVILEGES;
+
+-- change root password
+ALTER USER 'root'@'%' IDENTIFIED BY '123456';
+
+
+--  To verify the right permissions you just granted to the user, run this command:
+--    SHOW GRANTS FOR 'hliwa'@localhost;
+
+-- UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root';
+
 FLUSH PRIVILEGES;
--- mariadb/sqlscript.sh
+++ mariadb/sqlscript.sh
@@ -3,18 +3,8 @@
 echo "1- change configuration"
 cd /etc/mysql/mariadb.conf.d
 
-sed -i 's/^#port                   = 3306/port = 3306/' 50-server.cnf
+sed -i 's/#port/port/' 50-server.cnf
+sed -i 's/127.0.0.1/0.0.0.0/g' 50-server.cnf
 
 echo "----------configuration done-----------" 
 
-
-echo "2- creat sqluser"
-
-echo "CREATE DATABASE yourdb;" |  mariadb -u root
-echo "CREATE USER 'hliwa'@'localhost' IDENTIFIED BY 'secret';" | mariadb -u root 
-echo "GRANT ALL PRIVILEGES ON yourdb.* TO 'hliwa'@localhost;" | mariadb -u root
-# To verify the right permissions you just granted to the user, run this command:
-# echo "SHOW GRANTS FOR 'hliwa'@localhost;" | mariadb -u root -e
-echo "FLUSH PRIVILEGES;" | mariadb -u root
-
-echo "----------configuration done-----------"
