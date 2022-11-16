7e2e8ec56fe272af5b803224fb5dd4b7c5c3e1a9
lamvii <mili-1998@hotmail.fr>
2022-11-16 13:57:50 +0100

    nginx switch to debian

--- nginx/nginx_config.sh
+++ nginx/nginx_config.sh
@@ -5,24 +5,24 @@ service nginx start
 mkdir /etc/nginx/ssl
 openssl req -new -x509 -nodes -days 365 -newkey rsa:2048 \
 	-keyout /etc/nginx/ssl/selfsigned.key -out /etc/nginx/ssl/selfsigned.crt \
-	-subj "/C=Ma/ST=khouribga/L=aa/O=aa/OU=aa/CN=aa"
+	-subj "/C=Morooco/ST=StateProvince/L=Local/O=inception/OU=organization/CN=CommonName"
 
 
-cd etc/nginx/conf.d/
+cd etc/nginx/sites-enabled/
 
-# sed -i '7i		ssl_certificate /etc/nginx/ssl/selfsigned.crt;' default.conf
-# sed -i '8i		ssl_certificate_key /etc/nginx/ssl/selfsigned.key;' default.conf
-# sed -i '9i		ssl_session_timeout 1d;' default.conf
-# sed -i '10i		ssl_session_cache shared:SharedNixCraftSSL:10m; ' default.conf
-# sed -i '11i		ssl_session_tickets off;' default.conf
-# sed -i '12i' default.conf
-# sed -i '13i		# TLS 1.3 only' default.conf
-# sed -i '14i		ssl_protocols TLSv1.3;' default.conf
-# sed -i '15i		ssl_prefer_server_ciphers off;' default.conf
-# sed -i '16i' default.conf
+sed -i '24i		ssl_certificate /etc/nginx/ssl/selfsigned.crt;' default
+sed -i '25i		ssl_certificate_key /etc/nginx/ssl/selfsigned.key;' default
 
-# sed -i  's/listen 80 default_server;/listen 443 ssl http2;/' default.conf
-# sed -i 's/listen \[::\]:80 default_server;/listen \[::\]:443 ssl http2;/' default.conf
+
+sed -i '26i' default.conf
+
+sed -i '27i		# TLS 1.3 only' default
+sed -i '28i		ssl_protocols TLSv1.3;' default
+sed -i '29i		ssl_prefer_server_ciphers off;' default
+sed -i '30i' default
+
+sed -i 's/listen 80 default_server;/listen 443 ssl http2;/' default
+sed -i 's/listen \[::\]:80 default_server;/listen \[::\]:443 ssl http2;/' default
 
 
 # Make sure the nginx starts when system reboots:
