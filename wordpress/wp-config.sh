f22f79eb5766bb06101702258a19721b281ddac6
lamvii <mili-1998@hotmail.fr>
2022-11-23 19:29:47 +0100

    wp-cli works :)

--- wordpress/wp-config.sh
+++ wordpress/wp-config.sh
@@ -4,15 +4,19 @@ mkdir -p /run/php/
 
 cp wp-config-sample.php wp-config.php
 
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/;listen = \/run\/php\/php7.3-fpm.sock/'               /etc/php/7.3/fpm/pool.d/www.conf
+sed -i '37i listen = 0.0.0.0:9000'                                                                  /etc/php/7.3/fpm/pool.d/www.conf
+
 sed -i "s/define( 'DB_NAME', 'database_name_here' );/define( 'DB_NAME', 'yourdb' );/"               wp-config.php
 sed -i "s/define( 'DB_USER', 'username_here' );/define( 'DB_USER', '$MYSQL_USER' );/"               wp-config.php
 sed -i "s/define( 'DB_PASSWORD', 'password_here' );/define( 'DB_PASSWORD', '$MYSQL_PASSWORD' );/"   wp-config.php
 sed -i "s/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', 'mariadb' );/"                       wp-config.php
 
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/;listen = \/run\/php\/php7.3-fpm.sock/'               /etc/php/7.3/fpm/pool.d/www.conf
-sed -i '37i listen = 0.0.0.0:9000'                                                                  /etc/php/7.3/fpm/pool.d/www.conf
-
+#service php7.3-fpm start
 wp core --allow-root install --url="localhost" --title="inception" --admin_user=$MYSQL_USER \
-        --admin_password=$MYSQL_PASSWORD --admin_email="ael-idri@email.com"
+        --admin_password=$MYSQL_PASSWORD --admin_email="ael-idri@email.com" 
 
-/usr/sbin/php-fpm7.3 -F
+wp --allow-root theme activate twentytwentyone
+
+
+php-fpm7.3 -F
