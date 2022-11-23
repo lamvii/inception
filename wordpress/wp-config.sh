338aee356b5229da47e3ea5d84c820cf57f88464
lamvii <mili-1998@hotmail.fr>
2022-11-23 17:26:34 +0100

    wp-cli

--- wordpress/wp-config.sh
+++ wordpress/wp-config.sh
@@ -2,10 +2,19 @@
 
 mkdir -p /run/php/
 
-tar -xvzf latest.tar.gz
-mv wordpress/* ./
-rm -f latest.tar.gz index.html
+# tar -xvzf latest.tar.gz
+# mv wordpress/* ./
+# rm -f latest.tar.gz index.html
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+wp --allow-root core download
 cp wp-config-sample.php wp-config.php
+
+# echo $MYSQL_USER-------------------
+# echo $MYSQL_PASSWORD--------------------
 
 sed -i "s/define( 'DB_NAME', 'database_name_here' );/define( 'DB_NAME', 'yourdb' );/"               wp-config.php
 sed -i "s/define( 'DB_USER', 'username_here' );/define( 'DB_USER', '$MYSQL_USER' );/"               wp-config.php
@@ -14,3 +23,8 @@ sed -i "s/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', 'mariadb' );/"   
 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/;listen = \/run\/php\/php7.3-fpm.sock/'               /etc/php/7.3/fpm/pool.d/www.conf
 sed -i '37i listen = 0.0.0.0:9000'                                                                  /etc/php/7.3/fpm/pool.d/www.conf
+
+wp core --allow-root install --url="localhost" --title="inception" --admin_user=$MYSQL_USER \
+        --admin_password=$MYSQL_PASSWORD --admin_email="ael-idri@email.com"
+
+/usr/sbin/php-fpm7.3 -F
